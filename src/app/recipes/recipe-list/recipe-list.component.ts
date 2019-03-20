0b15215fdc62ae7bdae456e07c62f9eee0b2fca5
Alan Jones <a.jones@glofox.com>
2019-03-20 00:16:04 +0000

    basic setup of components layout and basic functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_640.jpg'),
+    new Recipe('Another Recipe', 'This is another test', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_640.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit() {
